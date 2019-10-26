3c6952ea2d29c3e27271e937d85428aa65c2da6e
Christopher Maree <christopher.maree@gmail.com>
2019-10-26 22:18:41 +0200

    Added dai to kyber mock

--- contracts/mocks/KyberNetworkProxyMock.sol
+++ contracts/mocks/KyberNetworkProxyMock.sol
@@ -2,7 +2,20 @@ pragma solidity ^0.5.7;
 
 import "../ERC20Interface.sol";
 
-contract KyberNetworkProxyMock {
+contract KyberNetworkProxyMock {    
+    uint expectedRate;
+    uint slippageRate;
+
+    ERC20 public daiContract;
+
+    constructor( uint _expectedRate, uint _slippageRate, address _daiTokenAddress) public{
+        expectedRate = _expectedRate;
+        slippageRate = _slippageRate;
+
+        daiContract = ERC20(_daiTokenAddress);
+    }
+    
+
     /// @notice use token address ETH_TOKEN_ADDRESS for ether
     /// @dev makes a trade between src and dest token and send dest token to destAddress
     /// @param src Src token
@@ -13,15 +26,6 @@ contract KyberNetworkProxyMock {
     /// @param minConversionRate The minimal conversion rate. If actual rate is lower, trade is canceled.
     /// @param walletId is the wallet ID to send part of the fees
     /// @return amount of actual dest tokens
-    
-    uint expectedRate;
-    uint slippageRate;
-
-    constructor( uint _expectedRate, uint _slippageRate) public{
-        expectedRate = _expectedRate;
-        slippageRate = _slippageRate;
-    }
-    
     function trade(
         ERC20 src,
         uint srcAmount,
@@ -35,7 +39,11 @@ contract KyberNetworkProxyMock {
         payable
         returns(uint)
     {
-        return 100;
+        uint256 tokensToSend = (minConversionRate * msg.value) / 10 ** 18;
+        daiContract.transfer(msg.sender, tokensToSend);
+
+        return tokensToSend;
+
     }
 
     function getExpectedRate(ERC20 src, ERC20 dest, uint srcQty)
