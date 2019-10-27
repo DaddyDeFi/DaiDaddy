03cf9828b9a3057ffb096bff83eeff3ec1dd57d1
sbcodes <stefan.bachmann123@gmail.com>
2019-10-27 18:13:39 +0100

    video test

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -4,10 +4,14 @@
       <img src="/img/bigMichaelLogo.png" class="logo" />
     </a-col>
     <a-col class="col-2" :md="10" :span="24">
-      <img src="/img/michaelBoxes.png" class="hero-michael" />
+      <!-- <img src="/img/michaelBoxes.png" class="hero-michael" /> -->
       <!-- <video class="MichaelVid" autoplay muted>
         <source src="anim/HeroMichaels.webm" type="video/webm" />
       </video>-->
+
+      <video class="MichaelVid" alt="step logo" style="height:600px;" autoplay="true">
+        <source src="anim/HeroMichaels.webm" type="video/webm" />
+      </video>
 
       <router-link to="/listing?open=true" class="cta" />
     </a-col>
