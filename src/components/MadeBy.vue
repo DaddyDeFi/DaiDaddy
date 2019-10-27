225bfa9a2e9eef2545d6fb450bd9610193e0ec88
sbcodes <stefan.bachmann123@gmail.com>
2019-10-27 22:18:23 +0100

    MadeBy About link added

--- src/components/MadeBy.vue
+++ src/components/MadeBy.vue
@@ -1,25 +1,75 @@
-<template>
-  <a-row type="flex" align="center">
-    <p class="mb">
-      <i class="em em-candy" />
-      <i class="em em-lollipop" /> Made with
-      <i class="em em-sparkling_heart" /> by DAI daddy
-      <i class="em em-tired_face" />
-      <i class="em em-sweat_drops" />
-    </p>
+ <template>
+  <a-row class="mb" type="flex" align="center">
+    <a-col :span="12">
+      <ul class="mb-list-left">
+        <li style="margin-right: 2.25rem;">
+          <a class="link" href="https://github.com/DaddyDeFi/DaiDaddy">
+            <i class="em em-candy" />
+            <i class="em em-lollipop" /> Made with
+            <i class="em em-sparkling_heart" /> by DAI daddy
+            <i class="em em-tired_face" />
+            <i class="em em-sweat_drops" />
+          </a>
+        </li>
+      </ul>
+    </a-col>
+    <a-col :span="12">
+      <ul class="mb-list-right">
+        <li class="mb-item">
+          <router-link class="link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </a-col>
   </a-row>
 </template>
 
-<script>
+ <script>
 export default {
   name: "MadeBy"
 };
 </script>
 
-<style scoped lang="scss">
+<style>
 .mb {
-  color: #473144;
+  padding: 50px;
+  padding-bottom: 20px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
   font-size: 20px;
-  text-align: center;
 }
-</style>
+
+.mb-list-left {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.mb-list-right {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.mb-item {
+  display: inline-block;
+  margin-right: 2.25rem;
+}
+
+.link {
+  color: #473144;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 25px;
+}
+.link:hover {
+  color: #ff1491;
+}
+</style> 
