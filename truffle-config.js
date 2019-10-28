453440fc5e44c8a8878df2d5fd2e0426558a5795
Christopher Maree <christopher.maree@gmail.com>
2019-10-28 09:20:57 +0200

    Added new build files

--- truffle-config.js
+++ truffle-config.js
@@ -3,11 +3,47 @@ const infuraApikey = '9542ce9f96be4ae08225dcde36ff1638';
 let mnemonic = require('./mnemonic');
 
 module.exports = {
+  mocha: {
+    useColors: true,
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 3
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.5.7',
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200 // Default: 200
+        },
+      }
+    }
+  },
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*', // Match any network id
+      gas: 6000000000,
+      gasPrice: 1
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01 // <-- Use this low gas price
+    },
+    live: {
+      provider: function () {
+        return new HDWalletProvider(require('./mnemonic_live.js'), `https://mainnet.infura.io/v3/${infuraApikey}`);
+      },
+      network_id: 1,
+      gas: 7000000, // default = 4712388
+      gasPrice: 6000000000 // default = 100 gwei = 100000000000
     },
     kovan: {
       provider: function () {
@@ -18,9 +54,10 @@ module.exports = {
       gasPrice: 10000000000 // default = 100 gwei = 100000000000
     }
   },
-  compilers: {
-    solc: {
-      version: "0.5.7"
-    }
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: 'FMJ6V8K8N3K6CDKAV6NGUHKMHT9434JKF4'
   }
-}
+};
